5933473f964e1eac71bff6c2546e4f1366ddced5
TypingOK <wjdwls717@gmail.com>
2023-05-30 15:48:04 +0900

    fix: prisma 타입 설정

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,32 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma";
 
-let prisma: PrismaClient;
+// export let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+const prismaClientPropertyName = `__prevent-name-collision__prisma`;
+type GlobalThisWithPrismaClient = typeof globalThis & {
+  [prismaClientPropertyName]: PrismaClient;
+};
+
+const getPrismaClient = () => {
+  if (process.env.NODE_ENV === `production`) {
+    return new PrismaClient();
+  } else {
+    const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
+    if (!newGlobalThis[prismaClientPropertyName]) {
+      newGlobalThis[prismaClientPropertyName] = new PrismaClient();
+    }
+    return newGlobalThis[prismaClientPropertyName];
   }
-  prisma = global.prisma;
-}
+};
+const prisma = getPrismaClient();
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
 
 export default prisma;
