efef1a52ed68c6931542be48ba32ad483d9a1ec9
TypingOK <wjdwls717@gmail.com>
2023-06-09 17:14:13 +0900

    fix: 배포 관련 수정

--- app/develope/[post]/page.tsx
+++ app/develope/[post]/page.tsx
@@ -14,28 +14,35 @@ const getPostDetail = async ({ id }: { id: string }) => {
       },
     },
   });
-  const converter = await remark().use(html).process(feed.content);
-  const content = converter.toString();
-  const { content: _, ...rest } = feed;
-  return {
-    ...rest,
-    content,
-    id,
-  };
+  if (feed !== null && feed !== undefined) {
+    const converter = await remark().use(html).process(feed.content);
+    const content = converter.toString();
+    const { content: _, ...rest } = feed;
+    return {
+      ...rest,
+      content,
+      id,
+    };
+  } else {
+    return undefined;
+  }
 };
 
 const Post = async ({ params: { post } }: { params: { post: string } }) => {
   const props = await getPostDetail({ id: post });
   console.log(props);
-
-  return (
-    <div>
-      <div>{props.title}</div>
-      <div>{props.createdAt.toString()}</div>
-      {/* <div>{props.tag}</div> */}
-      <div dangerouslySetInnerHTML={{ __html: props.content }}></div>
-    </div>
-  );
+  if (props) {
+    return (
+      <div>
+        <div>{props.title}</div>
+        <div>{props.createdAt.toString()}</div>
+        {/* <div>{props.tag}</div> */}
+        <div dangerouslySetInnerHTML={{ __html: props.content }}></div>
+      </div>
+    );
+  } else {
+    return <div>존재 하지 않는 페이지입니다.</div>;
+  }
 };
 
 export default Post;
