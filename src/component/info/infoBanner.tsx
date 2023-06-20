b9b14b9921ddaf1c7208526b8aa3c5f1009183f8
TypingOK <wjdwls717@gmail.com>
2023-06-20 23:35:17 +0900

    feat: 애니메이션 수정

--- src/component/info/infoBanner.tsx
+++ src/component/info/infoBanner.tsx
@@ -2,35 +2,55 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 
 const InfoBanner = () => {
+  const aControls = useAnimation();
+  const bControls = useAnimation();
+
+  const handleHoverStart = () => {
+    aControls.start({ x: 200 });
+    bControls.start({ x: 0 });
+  };
+  const handleHoverEnd = () => {
+    aControls.start({ x: 0 });
+    bControls.start({ x: 0 });
+  };
+
   return (
     <div className="w-full h-[300px] mt-5 mb-5 border-b-2">
       <Link href={"/info"}>
         <div className="flex w-full h-full relative overflow-hidden">
-          {/* <motion.div
-            whileHover={{ x: 1000 }}
+          <motion.div
+            // whileHover={{ x: 1000 }}
+            onHoverStart={handleHoverStart}
+            onHoverEnd={handleHoverEnd}
             transition={{ type: "spring", duration: 0.2, ease: "easeInOut" }}
-            className="w-[1920px] h-full absolute  right-[0px] flex justify-center"
+            className="w-full h-full absolute  right-[0px] flex justify-center"
           >
-            <div className="w-[1024px] bg-white h-full justify-center flex text-3xl">
-              알아보기
+            <motion.div
+              className="w-full text-3xl h-full absolute bg-white"
+              animate={aControls}
+              transition={{ duration: 0.3 }}
+            >
+              <div>알아보기</div>
+            </motion.div>
+            <motion.div className="w-full h-full absolute" animate={bControls}>
+              <div className="w-full h-full">
+                <h1 className="text-3xl">Author Info</h1>
+                <div>( 클릭시 내 정보 페이지로 이동합니다. )</div>
+              </div>
+            </motion.div>
+            <div className="relative w-2/6 h-full ml-auto">
+              <Image
+                src={"/사진.jpg"}
+                alt="내 사진"
+                width={400}
+                height={400}
+                className="bg-cover min-w-fit w-auto h-full ml-auto"
+              />
             </div>
-          </motion.div> */}
-          <div className="w-full h-full">
-            <h1 className="text-3xl">Author Info</h1>
-            <div>( 클릭시 내 정보 페이지로 이동합니다. )</div>
-          </div>
-          <div className="relative w-2/6 h-full ml-auto">
-            <Image
-              src={"/사진.jpg"}
-              alt="내 사진"
-              width={400}
-              height={400}
-              className="bg-cover min-w-fit w-auto h-full ml-auto"
-            />
-          </div>
+          </motion.div>
         </div>
       </Link>
     </div>
