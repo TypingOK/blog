074d82ac458e871baac8bbe2e519c6e662aa3efa
TypingOK <wjdwls717@gmail.com>
2023-11-03 12:29:28 +0900

    refactor:특정 페이지  크롤링 방지

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,4 +6,5 @@ module.exports = {
   sitemapSize: 7000, // sitemap별 최대 크기 (최대 크기가 넘어갈 경우 복수개의 sitemap으로 분리됨)
   changefreq: "daily", // 페이지 주소 변경 빈도 (검색엔진에 제공됨) - always, daily, hourly, monthly, never, weekly, yearly 중 택 1
   priority: 1, // 페이지 주소 우선순위 (검색엔진에 제공됨, 우선순위가 높은 순서대로 크롤링함)
+  exclude: ["/create", "/login"],
 };
